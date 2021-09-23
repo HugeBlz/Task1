8b57157cb04da9a7f5111d034f947d7aba7dbbd4
HugeBlz <90976954+HugeBlz@users.noreply.github.com>
2021-09-23 13:09:00 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 10;
+    int b = 05;
+    double sum;
+    sum = (double)a + (double)b/100.0;
+    printf("%f\n", sum);
+    return 0;
+}
+
